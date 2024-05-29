016b27f666b5446fdf51ec52cc3cac4d4b6b946e
sulhyun <sulhyun4788@naver.com>
2024-05-29 12:47:36 +0900

    자바 2일차 : 연습 예제

---
+++ java_HYUN/src/day02/Exam01.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		char ch = '0';
+		
+		System.out.println(ch);
+		
+	}
+	
+}
---
+++ java_HYUN/src/day02/Exam02.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 1, num2 = 2;
+		char ch = '+';
+		
+		/*
+		num1, num2, ch 를 이용하여 
+		1+2 가 콘솔에 출력되도록 코드를 
+		작성해보세요.
+		*/
+		
+		System.out.println(num1 + "" + ch + num2);
+	}
+
+}
