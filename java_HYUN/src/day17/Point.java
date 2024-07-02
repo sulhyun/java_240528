870b685c6a34799cb30b902a4e1f08abf0750f54
sulhyun <sulhyun4788@naver.com>
2024-07-02 19:34:54 +0900

    Update Point.java

--- java_HYUN/src/day17/Point.java
+++ java_HYUN/src/day17/Point.java
@@ -9,9 +9,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class Point implements Serializable{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 941182918428437876L;
 	
 	private int x,y;
