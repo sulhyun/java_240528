27f6edb10507dfa1fd13dd499b724e3eeff22299
sulhyun <sulhyun4788@naver.com>
2024-07-10 18:36:47 +0900

    css 5일차

--- java_HYUN/src/myproject/Schedule.java
+++ java_HYUN/src/myproject/Schedule.java
@@ -1,14 +1,71 @@
 package myproject;
 
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 import lombok.Data;
 
 @Data
-public class Schedule {
+public class Schedule implements Serializable, Comparable<Schedule>{
 
-	private Date date = new Date();		// 날짜
+	private static final long serialVersionUID = -5454249333392608158L;
+	
 	private String schedule;			// 일정
 	private String note;				// 상태
+	private Date date = new Date();		// 날짜
 	
+	public Schedule(String schedule, String note, String date) throws ParseException {
+		this.schedule = schedule;
+		this.note = note;
+		setDate(date);
+	}
+	
+	@Override
+	public String toString() {
+		return getDate() + " " + schedule + " " + note;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Schedule other = (Schedule) obj;
+		return Objects.equals(date, other.date);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(date);
+	}
+	
+	@Override
+	public int compareTo(Schedule o) {
+		return date.compareTo(o.date);
+	}	
+	
+	public void setDate(String date) throws ParseException {
+		SimpleDateFormat format = 
+				new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		this.date = format.parse(date);
+	}
+	
+	public String getDate() {
+		SimpleDateFormat format =
+				new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		return format.format(date);
+	}
+
+	
+	public void updateSchedule(String schedule, String note, String date) throws ParseException {
+		this.schedule = schedule;
+		this.note = note;
+		setDate(date);
+	}
 }
