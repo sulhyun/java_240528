1b755ff02d66c5e852169b0ba956094740ca722d
sulhyun <sulhyun4788@naver.com>
2024-07-19 00:09:26 +0900

    Create review01.java

---
+++ java_HYUN/src/review/review01.java
@@ -0,0 +1,14 @@
+package review;
+
+import java.util.Calendar;
+
+public class review01 {
+
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		System.out.println(Calendar.DAY_OF_WEEK);
+		
+	}
+		
+}
