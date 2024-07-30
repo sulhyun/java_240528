9a2f5f5235fff2e09a86bd3e6986b40a2dcdfde0
sulhyun <sulhyun4788@naver.com>
2024-07-30 17:49:59 +0900

    Database 8일차 : 쇼핑몰 사이트

--- db/07일차_DML_INSERT_쇼핑몰 사이트.sql
+++ db/07일차_DML_INSERT_쇼핑몰 사이트.sql
@@ -8,4 +8,78 @@ INSERT INTO CATEGORY(CA_NAME) VALUES
 INSERT INTO MEMBER (ME_ID, ME_PW, ME_EMAIL, ME_PHONE, ME_AUTHORITY, ME_FAIL) VALUES
 ('abc123', 'abc123', 'abc123@naver.com', '011-1234-5678', 'USER', 0),
 ('qwe123', 'qwe123', 'qwe123@naver.com', '011-1111-2222', 'USER', 0);
- 
+
+# 제품 등록하는 쿼리
+# 분류 : 옷, 코드 : CLO001, 상품명 : 시원한 티셔츠, 가격 : 20000, 내용 : 티셔츠를 입으면 시원해요.
+# 분류 : 옷, 코드 : CLO002, 상품명 : 시원한 반바지, 가격 : 15000, 내용 : 여름 전용 반바지입니다.
+# 분류 : 옷, 코드 : CLO003, 상품명 : 시원한 양말, 가격 : 20000, 내용 : 시원한 의류 시리즈 마지막 용품입니다.
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'CLO001', '시원한 티셔츠', '티셔츠를 입으면 시원해요.', '20000', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '옷';
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'CLO002', '시원한 반바지', '여름 전용 반바지입니다.', '15000', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '옷';
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'CLO003', '시원한 양말', '시원한 의류 시리즈 마지막 용품입니다.', '20000', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '옷';
+
+# 분류 : 모자, 코드 : CAP001, 상품명 : 여름 모자, 가격 : 30000, 내용 : 그늘을 만들어줘요.
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'CAP001', '여름 모자', '그늘을 만들어줘요.', '30000', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '모자';
+
+# 분류 : 신발, 코드 : SHO001, 상품명 : 싼 크록스, 가격 : 19999, 내용 : 인기 신발.
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'SHO001', '싼 크록스', '인기 신발.', '19999', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '신발';
+
+# 분류 : 악세서리, 코드 : ACC001, 상품명 : 금 목걸이, 가격 : 100000, 내용 : 부의 상징.
+INSERT INTO PRODUCT(PR_CODE, PR_NAME, PR_CONTENT, PR_PRICE, PR_CA_NUM)
+SELECT 'ACC001', '금 목걸이', '부의 상징.', '100000', CA_NUM 
+FROM CATEGORY 
+WHERE CA_NAME = '악세서리';
+
+# abc123회원이 CLO001번 제품을 장바구니에 3개 담았을 때 쿼리
+INSERT INTO BASKET VALUES
+(NULL, 'CLO001', 'abc123', 3);
+
+# abc123회원이 1번 제품을 장바구니에 2개 담았을 때 쿼리
+UPDATE BASKET SET BA_AMOUNT = 2 WHERE BA_PR_CODE = 'CLO001' AND BA_ME_ID = 'abc123';
+
+# abc123회원이 ACC001 제품을 장바구니에 1개 담았을 때 쿼리
+INSERT INTO BASKET VALUES
+(NULL, 'ACC001', 'abc123', 1);
+
+# abc123회원이 장바구니에 있는 모든 제품을 구매했을 때 필요한 모든 쿼리
+INSERT INTO BUY(BU_PR_CODE, BU_ME_ID, BU_AMOUNT, BU_STATE, BU_DATE) VALUES
+('CLO001', 'abc123', 2, '구매', NOW()),
+('ACC001', 'abc123', 1, '구매', NOW());
+
+# 장바구니에서 구매를 하면 장바구니에 있는 항목을 제거해야 함
+DELETE FROM BASKET WHERE BA_PR_CODE = 'CLO001' AND BA_ME_ID = 'abc123';
+DELETE FROM BASKET WHERE BA_PR_CODE = 'ACC001' AND BA_ME_ID = 'abc123';
+
+# abc123회원이 id : abc123, pw : abc1234로 로그인 시도했을 때 실행해야하는 쿼리
+# => 로그인 실패 => 로그인 실패횟수 증가
+UPDATE MEMBER SET ME_FAIL = ME_FAIL + 1 WHERE ME_ID = 'abc123';
+
+# abc123회원이 로그인 시도가 성공했을 때 싱행해야하는 쿼리
+UPDATE MEMBER SET ME_FAIL = 0 WHERE ME_ID = 'abc123';
+
+# abc123회원이 비번 찾기를 시도해서 인증코드가 123asd가 발급됐을 때 실행되야하는 쿼리
+INSERT INTO CODE VALUES
+('abc123', '123asd', DATE_ADD(NOW(), INTERVAL 5 MINUTE));
+
+# abc123회원이 인증코드를 입력해서 비번을 abc1234로 수정했을 때 실행해야하는 쿼리
+UPDATE MEMBER SET ME_PW = 'abc1234' WHERE ME_ID = 'abc123';
+
+# 인증코드를 통해 비번을 변경했으면 인증코드를 삭제
+DELETE FROM CODE WHERE CO_ME_ID = 'abc123';
+
+
+
--- db/07일차_DML_SELECT_쇼핑몰 사이트.sql
+++ db/07일차_DML_SELECT_쇼핑몰 사이트.sql
@@ -1,9 +1,72 @@
 # 분류별 등록된 제품수를 조회하는 쿼리
 SELECT
-	CA_NAME 분류, COUNT(PR_CA_NUM) 제품수
+	CA_NAME 분류, COUNT(PR_CODE) 제품수
+FROM
+	PRODUCT
+RIGHT JOIN
+	CATEGORY ON CA_NUM = PR_CA_NUM
+GROUP BY CA_NUM;
+
+# 분류 중에서 옷을 클릭했을 때 => 옷으로 등록된 제품들을 조회하는 쿼리
+SELECT CA_NAME 분류, PR_NAME 제품명, PR_CONTENT 제품설명, PR_PRICE 가격
 FROM
 	PRODUCT
 JOIN
 	CATEGORY ON CA_NUM = PR_CA_NUM
-GROUP BY CA_NAME
-	
+WHERE CA_NUM = 1;
+
+# '시원한'이 들어간 제품을 검색 => 제목에 '시원한'이 포함된 제품을 조회하는 쿼리
+SELECT PR_NAME 제품명, PR_CONTENT 제품설명, PR_PRICE 가격
+FROM
+	PRODUCT
+WHERE PR_NAME LIKE CONCAT('%', '시원한', '%');
+
+# 누적 판매량이 가장 많은 제품들을 조회하는 쿼리
+SELECT 
+	DENSE_RANK() OVER(ORDER BY SUM(BU_AMOUNT) DESC) 순위, PRODUCT.*, SUM(BU_AMOUNT) 판매량
+FROM
+	PRODUCT
+LEFT JOIN 
+	(SELECT * FROM BUY WHERE BU_STATE IN('구매', '구매확정')) B ON PR_CODE = BU_PR_CODE
+GROUP BY PR_CODE;
+
+# 옷 제품들 중에서 가격이 높은 순으로 제품을 조회하는 쿼리
+SELECT
+	PRODUCT.*
+FROM
+	PRODUCT
+JOIN
+	CATEGORY ON PR_CA_NUM = CA_NUM
+WHERE
+	CA_NAME = '옷'
+ORDER BY PR_PRICE DESC;
+
+# abc123회원의 구매 목록을 조회하는 쿼리
+SELECT DISTINCT 
+	BU_ME_ID 아이디, BU_PR_CODE 제품코드, BU_STATE 상태,
+	PR_NAME 제품명, PR_PRICE 가격, BU_AMOUNT 구매횟수
+FROM
+	BUY
+JOIN
+	PRODUCT ON PR_CODE = BU_PR_CODE
+WHERE BU_ME_ID = 'abc123' AND BU_STATE IN('구매', '구매확정');
+
+# abc123회원의 누적 구매 금액을 조회하는 쿼리
+SELECT BU_ME_ID, FORMAT(SUM(PR_PRICE * BU_AMOUNT), 0) '누적 금액'
+FROM
+	BUY
+JOIN 
+	PRODUCT ON PR_CODE = BU_PR_CODE
+WHERE
+	BU_ME_ID = 'abc123' AND BU_STATE IN('구매', '구매확정')
+GROUP BY BU_ME_ID;
+
+# 회원별 누적 금액을 조회하는 쿼리
+SELECT ME_ID, IFNULL(FORMAT(SUM(PR_PRICE * BU_AMOUNT), 0), 0) '누적 금액'
+FROM
+	MEMBER
+LEFT JOIN
+	(SELECT * FROM BUY WHERE BU_STATE IN('구매', '구매확정')) B ON ME_ID = BU_ME_ID
+LEFT JOIN 
+	PRODUCT ON PR_CODE = BU_PR_CODE
+GROUP BY ME_ID;
