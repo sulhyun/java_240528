64abada1b8935dd62cc900db57c81bd64b175bb9
sulhyun <sulhyun4788@naver.com>
2024-08-20 17:51:52 +0900

    Servlet 8일차 : 커뮤니티 복습

---
+++ servlet/servlet2/src/main/java/kr/kh/app/model/vo/MemberVO.java
@@ -0,0 +1,28 @@
+package kr.kh.app.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberVO {
+	
+	private String me_id; 
+	private String me_pw; 
+	private String me_email; 
+	private String me_authority; 
+	private int me_fail; 
+	private String me_cookie; 
+	private Date me_limit; 
+	private int me_report; 
+	private String me_ms_name; 
+	private Date me_stop;
+	
+	public MemberVO(String me_id, String me_pw, String me_email) {
+		this.me_id = me_id;
+		this.me_pw = me_pw;
+		this.me_email = me_email;
+	}
+}
