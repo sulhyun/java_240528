d6c1700e3e1a09b3a16c5c5c50b8731f37094998
sulhyun <sulhyun4788@naver.com>
2024-08-26 12:30:02 +0900

    스프링 1일차 : DB연동 및 테스트

---
+++ spring_project/spring_s/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -0,0 +1,29 @@
+package kr.kh.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.spring.model.dto.PersonDTO;
+import kr.kh.spring.service.MemberService;
+
+@Controller
+public class HomeController {
+	
+	// private MemberSerivce memberService = new MemberServiceImp();
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	// 화면에서 보낸 정보를 객체로 받는 경우 실행 과정
+	// 1. 해당 클래스의 기본 생성자가 호출
+	// 2. 화면에서 보낸 name과 같은 멤버변수들의 setter를 호출해서 값을 변경
+	public String home(Model model, PersonDTO person) {
+		System.out.println(person);
+		model.addAttribute("name", "홍길동");
+		return "home";
+	}
+	
+}
---
+++ spring_project/spring_s/src/main/java/kr/kh/spring/model/dto/PersonDTO.java
@@ -0,0 +1,12 @@
+package kr.kh.spring.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PersonDTO {
+	
+	private String name;
+	private int age;
+}
