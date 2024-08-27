ba6d527734f60edcd6160f20ca069e615abdc50f
sulhyun <sulhyun4788@naver.com>
2024-08-27 11:16:13 +0900

    스프링 2일차 : DB연동 완료

---
+++ spring_project/spring2/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -0,0 +1,25 @@
+package kr.kh.spring.controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.spring.service.MemberService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "home";
+	}
+	
+}
