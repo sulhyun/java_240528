29a2bf546934549c8fbe6a87a8bf3ea2d8190209
sulhyun <sulhyun4788@naver.com>
2024-08-29 16:03:36 +0900

    스프링 4일차 : 주석 수정

--- spring_project/spring_s/src/main/java/kr/kh/spring/pagination/Criteria.java
+++ spring_project/spring_s/src/main/java/kr/kh/spring/pagination/Criteria.java
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Criteria {
 	
-	protected int page = 1;//현재 페이지 : 기본값 - 1
-	protected int perPageNum = 10;//한 페이지에서 컨텐츠 개수 : 기본값 - 10
+	protected int page = 1; // 현재 페이지 : 기본값 - 1
+	protected int perPageNum = 10; // 한 페이지에서 컨텐츠 개수 : 기본값 - 10
 	
-	protected String search = ""; //검색어 : 기본값 - 빈문자열=> 전체 검색
-	protected String type; //검색 타입 
+	protected String search = ""; // 검색어 : 기본값 - 빈문자열=> 전체 검색
+	protected String type; // 검색 타입 
 	
 	public Criteria(int page, int perPageNum) {
 		this.page = page;
