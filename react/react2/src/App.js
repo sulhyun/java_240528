bd7b031d3527be1b2475d7c721163839de4709ee
sulhyun <sulhyun4788@naver.com>
2024-09-09 16:35:37 +0900

    리액트 1일차 : 예제 - 오늘의 할일 목록

--- react/react2/src/App.js
+++ react/react2/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Test1 from './test1/Test1';
+import Todo from './Todo/Todo';
+import TodoList from './TodoList/TodoList';
 
 function App() {
   return (
     <div>
       {/* <Test1 /> */}
+      {/* <Todo /> */}
+      <TodoList />
     </div>
   );
 }
