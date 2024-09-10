cfeff12cf505cac0e073334837c62cc8081b44b2
sulhyun <sulhyun4788@naver.com>
2024-09-10 16:41:50 +0900

    리액트 2일차 : 예제 - select 활용 예제

--- react/react2/src/App.js
+++ react/react2/src/App.js
@@ -4,6 +4,7 @@ import ButtonBox2 from './buttonEx/ButtonBox2';
 import ButtonBox3 from './buttonEx/ButtonBox3';
 import Input1 from './input/input1';
 import Input2 from './input/input2';
+import Select from './input/Select';
 import List1 from './list/List1';
 import Test1 from './test1/Test1';
 import Todo from './Todo/Todo';
@@ -21,6 +22,7 @@ function App() {
       {/* <Input1 /> */}
       {/* <Input2 /> */}
       {/* <List1 /> */}
+      <Select />
     </div>
   );
 }
---
+++ react/react2/src/input/Select.js
@@ -0,0 +1,32 @@
+
+/*
+Select 태그를 이용해서 과일은 선택할 수 있는 창을 만들고,
+과일을 선택하면 h1태그에 선택한 과일이 출력되도록 작성
+- 선택안함, 사과, 바나나, 오렌지
+*/
+
+import { useState } from "react";
+
+function Select(){
+
+	let [fruit, setFruit] = useState("");
+
+	function display(){
+		return fruit == '' ? '' : fruit;
+	}
+	
+	return(
+		<div>
+			<select onChange={(e)=>setFruit(e.target.value)}>
+				<option value={""}>선택안함</option>
+				<option>사과</option>
+				<option>바나나</option>
+				<option>오렌지</option>
+			</select>
+			<h1>{fruit == '' ? '' : fruit}</h1>
+			<h1>{display()}</h1>
+		</div>
+	)
+}
+
+export default Select;
