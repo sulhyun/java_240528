bced936dd75771d0dc61be4c191434f9d6c547a1
sulhyun <sulhyun4788@naver.com>
2024-09-11 16:11:40 +0900

    리액트 3일차 : 커뮤니티 기능 구현

--- react/react2/src/App.js
+++ react/react2/src/App.js
@@ -6,6 +6,7 @@ import Input1 from './input/input1';
 import Input2 from './input/input2';
 import Select from './input/Select';
 import List1 from './list/List1';
+import PostEx from './post1/PostEx';
 import Test1 from './test1/Test1';
 import Todo from './Todo/Todo';
 import TodoList from './TodoList/TodoList';
@@ -22,7 +23,8 @@ function App() {
       {/* <Input1 /> */}
       {/* <Input2 /> */}
       {/* <List1 /> */}
-      <Select />
+      {/* <Select /> */}
+      <PostEx />
     </div>
   );
 }
---
+++ react/react2/src/post1/PostEx.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import Modal from './Modal';
+import './PostEx.css';
+import PostInsert from './PostInsert';
+import PostList from './PostList';
+/* 
+- 게시글 제목, 내용, 작성자를 입력해서 버튼을 클릭하면
+  입력한 게시글 정보가 콘솔창에 출력되도록 작성
+	1. state 변수 3개 추가(title, writer, content)
+	2. 내용이 바뀔때마다 변경된 내용을 각 항목에 업데이트
+	3. 버튼을 클릭하면 title, writer, content를 콘솔에 출력
+*/
+function PostEx(){
+	
+	let [list, setList] = useState([{
+		title : '공지',
+		writer : 'admin',
+		content : '공지사항',
+		view : 0
+	}]);
+	
+	return(
+		<div>
+			<PostInsert list={list} setList={setList} />
+			<hr/>
+			<PostList list={list} setList={setList}/>
+		</div>
+	)
+}
+
+export default PostEx;
