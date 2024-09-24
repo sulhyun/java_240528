ac74261d0e89b9e80a0d5424bd6e49050801b2fc
sulhyun <sulhyun4788@naver.com>
2024-09-24 16:05:58 +0900

    스푸링 부트 1일차 : 레이아웃 적용

--- spring_boot_project/boot/src/main/java/kr/kh/boot/controller/MainController.java
+++ spring_boot_project/boot/src/main/java/kr/kh/boot/controller/MainController.java
@@ -1,13 +1,21 @@
 package kr.kh.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/")
-	public String home() {
-		return "index";
+	public String home(Model model) {
+		model.addAttribute("name", "홍길동");
+		return "home";
+	}
+	
+	@GetMapping("/home2")
+	public String home2(Model model) {
+		model.addAttribute("name", "홍길동");
+		return "home2";
 	}
 }
