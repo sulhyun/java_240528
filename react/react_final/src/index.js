b2d32e0963c0113873e34831c14a9c354ea96289
sulhyun <sulhyun4788@naver.com>
2024-09-24 15:22:52 +0900

    스프링 부트 1일차 : 스프링 부트 프로젝트 생성 및 기본 설정

--- react/react_final/src/index.js
+++ react/react_final/src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import App2 from './App2';
+import App3 from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <App />
-  <App2 />
+  // <App2 />
+  <App3 />
 );
