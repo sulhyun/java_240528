6d9a10ad6fc2de378723931221d7abf1f162c230
sulhyun <sulhyun4788@naver.com>
2024-09-25 11:12:42 +0900

    스프링 부트 2일차 : 게시글 목록 조회 기능 구현

--- spring_boot_project/boot/src/main/java/kr/kh/boot/controller/PostController.java
+++ spring_boot_project/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -1,10 +1,14 @@
 package kr.kh.boot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import kr.kh.boot.model.vo.CommunityVO;
+import kr.kh.boot.model.vo.PostVO;
 import kr.kh.boot.service.PostService;
 import lombok.AllArgsConstructor;
 
@@ -12,13 +16,16 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class PostController {
 
-	@Autowired
 	private PostService postService;
 	
 	@GetMapping("/post/list/{co_num}")
-	public String postList(@PathVariable int co_num) {
-		System.out.println(co_num);
-		
+	public String postList(Model model, @PathVariable int co_num) {
+		// 커뮤니티 번호에 맞는 게시글 목록을 가져와서 화면에 전달
+		List <PostVO> list = postService.getPostList(co_num);
+		// 커뮤니티 목록을 가져와서 화면에 전달
+		List <CommunityVO> communities = postService.getCommunityList();
+		model.addAttribute("list", list);
+		model.addAttribute("communities", communities);
 		return "/post/list";
 	}
 }
--- spring_boot_project/boot/src/main/java/kr/kh/boot/service/PostService.java
+++ spring_boot_project/boot/src/main/java/kr/kh/boot/service/PostService.java
@@ -1,14 +1,26 @@
 package kr.kh.boot.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kr.kh.boot.dao.PostDAO;
+import kr.kh.boot.model.vo.CommunityVO;
+import kr.kh.boot.model.vo.PostVO;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class PostService {
 
-	@Autowired
-	PostDAO postDao;
+	private PostDAO postDao;
+
+	public List<PostVO> getPostList(int co_num) {
+		return postDao.selectPostList(co_num);
+	}
+
+	public List<CommunityVO> getCommunityList() {
+		return postDao.selectCommunityList();
+	}
 	
 }
