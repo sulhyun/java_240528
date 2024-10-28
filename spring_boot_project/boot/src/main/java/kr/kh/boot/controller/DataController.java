cffe0dfa54f2cc5656f451daba5f86c0b74b407a
sulhyun <sulhyun4788@naver.com>
2024-10-29 08:32:10 +0900

    API

---
+++ spring_boot_project/boot/src/main/java/kr/kh/boot/controller/DataController.java
@@ -0,0 +1,54 @@
+package kr.kh.boot.controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DataController {
+	
+	@GetMapping(value="/data/sample")
+	public String DataSample() {
+		return "sample";
+	}
+	
+	
+	@ResponseBody
+	@PostMapping(value="/data/sample", produces="application/text; charset=UTF-8;")
+	public String dataSamplePost() throws Exception {
+		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"); /*URL*/
+		urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=i332qUKvV4ktNv2CiwkL8YuNf9ujF19Mgu7484Jdfej4U368P9PoQD8iXi747HBzDcBss1cywCRKH2O3ll5njw%3D%3D"); /*Service Key*/
+		urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml 또는 json*/
+		urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+		urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+		urlBuilder.append("&" + URLEncoder.encode("sidoName","UTF-8") + "=" + URLEncoder.encode("서울", "UTF-8")); /*시도 이름(전국, 서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종)*/
+		urlBuilder.append("&" + URLEncoder.encode("ver","UTF-8") + "=" + URLEncoder.encode("1.0", "UTF-8")); /*버전별 상세 결과 참고*/
+		URL url = new URL(urlBuilder.toString());
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		System.out.println("Response code: " + conn.getResponseCode());
+		    BufferedReader rd;
+		    if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+		        rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		    } else {
+		        rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		    }
+		    StringBuilder sb = new StringBuilder();
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		        sb.append(line);
+		    }
+		    rd.close();
+		    conn.disconnect();
+		    System.out.println(sb.toString());
+		return sb.toString();
+	}
+}
