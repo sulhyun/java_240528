d40de68147da0b15c96652d441da9d0987569185
sulhyun <sulhyun4788@naver.com>
2025-12-30 20:12:51 +0900

    스프링 프로젝트 템플릿 설정

--- spring_project/spring_react/src/main/java/kr/kh/spring_react/HomeController.java
+++ spring_project/spring_react/src/main/java/kr/kh/spring_react/HomeController.java
@@ -22,24 +22,7 @@ import kr.kh.spring_react.service.MemberService;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
 	@Autowired
 	MemberService memberService;
 
