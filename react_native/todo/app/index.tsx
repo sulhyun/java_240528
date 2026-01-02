dce8b3c9207db74ff1ec0da7032e31249af8cf32
sulhyun <sulhyun4788@naver.com>
2026-01-02 20:32:21 +0900

    Todo List 추가 기능 구현

--- react_native/todo/app/index.tsx
+++ react_native/todo/app/index.tsx
@@ -1,8 +1,75 @@
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+type Todo = {
+  id: number,
+  text: string
+}
+
 export default function Index() {
+  const [text, setText] = useState("");
+  const [todoList, SetTodoList] = useState<Todo[]>([]);
+  const [id, setId] = useState(1);
+
+  const onChangeText = (content: string) => {
+    setText(content);
+  }
+
+  // 추가
+  const addTodo = () => {
+    const tmp = {
+      id : id,
+      text : text
+    }
+    SetTodoList([...todoList, tmp]);
+    setText("");
+    setId((prev) => prev + 1);
+  }
 
   return (
     <>
-
+      <View style={styles.contains}>
+        <Text style={styles.title}>My Todo</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} value={text} onChangeText={onChangeText} />
+          <TouchableOpacity style={styles.button} onPress={addTodo}>
+            <Text style={styles.buttonText}>추가</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text>{text}</Text>
+        </View>
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  contains : {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 15,
+  },
+  title : {
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  input : {
+    padding: 10, 
+    flex: 1,
+  },
+  button : {
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  buttonText : {
+    fontWeight: 'bold',
+  },
+  inputContainer : {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
+  }
+})
