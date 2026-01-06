1d7d35f395635d0268af3a3456181d50d140fa1a
sulhyun <sulhyun4788@naver.com>
2026-01-06 17:48:28 +0900

    Example ...

--- react_native/example/app/(tabs)/(post)/index.tsx
+++ react_native/example/app/(tabs)/(post)/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={ styles.container }>
       <Text>Edit app/post.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container : { flex: 1, justifyContent: "center", alignItems: "center" }
+});
