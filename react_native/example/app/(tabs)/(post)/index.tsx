b9c0921d9ec08c191c7905b5011fbd8baa8d092d
sulhyun <sulhyun4788@naver.com>
2026-01-06 17:33:14 +0900

    Example 하단 탭 구현

--- react_native/example/app/(tabs)/(post)/index.tsx
+++ react_native/example/app/(tabs)/(post)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/todo.tsx to edit this screen.</Text>
+      <Text>Edit app/post.tsx to edit this screen.</Text>
     </View>
   );
 }
---
+++ react_native/example/app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return(
+    <Tabs screenOptions={{ headerShown: false}}>
+      <Tabs.Screen 
+        name="(todo)"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="list"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen 
+        name="(post)"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="bookmarks"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          )
+        }}
+      />
+    </Tabs>
+  )
+}
