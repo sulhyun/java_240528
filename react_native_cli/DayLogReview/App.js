22e75e0662e714e9298d7e6256b599a1cf25465e
sulhyun <sulhyun4788@naver.com>
2026-01-19 15:59:43 +0900

    책 예제 복습 Context API 사용

--- react_native_cli/DayLogReview/App.js
+++ react_native_cli/DayLogReview/App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from './screens/RootStack';
+import LogContext from './contexts/LogContext';
 
 function App() {
   return (
     <NavigationContainer>
-      <RootStack />
+      <LogContext.Provider value="반갑습니다111">
+        <RootStack />
+      </LogContext.Provider>
     </NavigationContainer>
   );
 }
--- react_native_cli/DayLogReview/screens/FeedsScreen.js
+++ react_native_cli/DayLogReview/screens/FeedsScreen.js
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import { Text, View, Button } from "react-native";
+import LogContext from "../contexts/LogContext";
 
 function FeedsScreen({navigation}) {
+  const value = useContext(LogContext);
+
   return(
     <View>
       <Text>Feed</Text>
       <Button title="Write 이동" onPress={() => navigation.push('Write',{name: '홍길동'})} />
+      <Text>{value}</Text>
     </View>
   );
 };
