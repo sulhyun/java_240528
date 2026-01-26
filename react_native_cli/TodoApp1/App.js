62531502486a348c438c13f74489ebe570bbe308
sulhyun <sulhyun4788@naver.com>
2026-01-26 21:46:36 +0900

    리액트 네이티브 복습 2일차

---
+++ react_native_cli/TodoApp1/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+import DateHead from './components/DateHead';
+import AppTodo from './components/AppTodo';
+import Empty from './components/Empty';
+
+function App() {
+  const today = new Date();
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <KeyboardAvoidingView
+          style={styles.avoid}
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+          <DateHead date={today} />
+          <Empty />
+          <AppTodo />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  },
+});
+
+export default App;
