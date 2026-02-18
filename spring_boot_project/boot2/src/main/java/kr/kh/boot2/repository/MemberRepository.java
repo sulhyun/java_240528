8ed394a596c42fb99b36785f0c6d39e0de423f07
sulhyun <sulhyun4788@naver.com>
2026-02-18 13:24:23 +0900

    스프링 부트 복습 2일차

--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/repository/MemberRepository.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/repository/MemberRepository.java
@@ -9,7 +9,7 @@ public interface MemberRepository {
 
 	Member save(Member member);
 	Optional<Member> findById(Long id);
-	Optional<Member> findBtName(String name);
+	Optional<Member> findByName(String name);
 	List<Member> findAll();
 	
 }
