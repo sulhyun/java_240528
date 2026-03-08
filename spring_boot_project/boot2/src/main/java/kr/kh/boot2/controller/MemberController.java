567d357573b66881246a0222dbab5e12bde9ded4
sulhyun <sulhyun4788@naver.com>
2026-03-08 13:28:20 +0900

    스프링 부트 복습 3일차

--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/controller/MemberController.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/controller/MemberController.java
@@ -1,7 +1,10 @@
 package kr.kh.boot2.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -31,5 +34,12 @@ public class MemberController {
 		member.setName(form.getName());
 		memberService.join(member);
 		return "redirect:/";
+	}
+	
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
