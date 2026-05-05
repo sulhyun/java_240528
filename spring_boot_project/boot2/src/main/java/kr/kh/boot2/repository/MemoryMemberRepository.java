44fe103ba754c52b3bddb2bfc5ee752d6c63e76c
sulhyun <sulhyun4788@naver.com>
2026-05-05 21:48:54 +0900

    스프링 부트 복습 5일차

--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/repository/MemoryMemberRepository.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/repository/MemoryMemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import kr.kh.boot2.domain.Member;
 
+// 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
 public class MemoryMemberRepository implements MemberRepository {
 	
 	private static Map<Long, Member> store = new HashMap<Long, Member>(); 	// 회원 저장소
---
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/service/MemberService.java
@@ -0,0 +1,53 @@
+package kr.kh.boot2.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import kr.kh.boot2.domain.Member;
+import kr.kh.boot2.repository.MemberRepository;
+
+@Service
+public class MemberService {
+	
+	private final MemberRepository memberRepository;
+	
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	/**
+	 * 회원가입 
+	 */
+	public Long join(Member member) {
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	/**
+	 * 중복 회원 검증
+	 */
+	private void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+			.ifPresent(m -> {
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
+	}
+	
+	/**
+	 * 전체 회원 조회
+	 */
+	public List<Member> findMembers() {
+		return 	memberRepository.findAll();
+	}
+	
+	/**
+	 * 특정 회원 조회
+	 */
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+	
+}
