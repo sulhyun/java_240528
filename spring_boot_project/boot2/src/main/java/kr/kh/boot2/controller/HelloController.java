ea0a5099e63ec37316e20ec4f09396542a0196f4
sulhyun <sulhyun4788@naver.com>
2026-05-05 15:11:22 +0900

    스프링 부트 복습 5일차

---
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/controller/HelloController.java
@@ -0,0 +1,22 @@
+package kr.kh.boot2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("hello")
+	public String hello(Model model) {
+		model.addAttribute("data", "Hello!!");
+		return "hello";
+	}
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
+	}
+}
