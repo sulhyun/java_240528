74685a00cf71bbd95147e29e566bbf830ddd4ddb
sulhyun <sulhyun4788@naver.com>
2026-05-05 20:37:09 +0900

    스프링 부트 복습 5일차

---
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/repository/MemoryMemberRepository.java
@@ -0,0 +1,43 @@
+package kr.kh.boot2.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import kr.kh.boot2.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+	
+	private static Map<Long, Member> store = new HashMap<Long, Member>(); 	// 회원 저장소
+	private static long sequence = 0L;										// 회원 ID
+	
+	@Override
+	public Member save(Member member) {
+		member .setId(++sequence);
+		store.put(member.getId(), member);
+		return member;
+	}
+
+	@Override
+	public Optional<Member> findById(Long id) {
+		return Optional.ofNullable(store.get(id));
+	}
+
+	@Override
+	public Optional<Member> findByName(String name) {
+		return store.values().stream()
+				.filter(member -> member.getName().equals(name))
+				.findAny();
+	}
+
+	@Override
+	public List<Member> findAll() {
+		return new ArrayList<>(store.values());
+	}
+
+	public void clearStore() {
+		store.clear();
+	}
+}
