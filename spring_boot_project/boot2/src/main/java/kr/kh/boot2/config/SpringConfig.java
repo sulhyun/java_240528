44cfde3d8da372908d3697c5081f506e0c0ff82b
sulhyun <sulhyun4788@naver.com>
2026-05-10 12:06:59 +0900

    스프링 부트 복습 6일차

--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/config/SpringConfig.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/config/SpringConfig.java
@@ -1,15 +1,25 @@
 package kr.kh.boot2.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import kr.kh.boot2.repository.JdbcMemberRepository;
 import kr.kh.boot2.repository.MemberRepository;
-import kr.kh.boot2.repository.MemoryMemberRepository;
 import kr.kh.boot2.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+	
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
@@ -17,7 +27,8 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 
 }
