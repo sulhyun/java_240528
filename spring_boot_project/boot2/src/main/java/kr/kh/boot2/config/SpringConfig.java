a34ae2f1c81e8a4593b6fbf52079911ce22f55f8
sulhyun <sulhyun4788@naver.com>
2026-05-10 22:52:31 +0900

    스프링 부트 복습 6일차

--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/config/SpringConfig.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/config/SpringConfig.java
@@ -6,18 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import jakarta.persistence.EntityManager;
 import kr.kh.boot2.repository.JdbcMemberRepository;
+import kr.kh.boot2.repository.JdbcTemplateMemberRepositrory;
+import kr.kh.boot2.repository.JpaMemberRepository;
 import kr.kh.boot2.repository.MemberRepository;
 import kr.kh.boot2.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
-	private DataSource dataSource;
+	private EntityManager em;
 	
 	@Autowired
-	public SpringConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public SpringConfig(EntityManager em) {
+		this.em = em;
 	}
 	
 	@Bean
@@ -28,7 +31,9 @@ public class SpringConfig {
 	@Bean
 	public MemberRepository memberRepository() {
 		//return new MemoryMemberRepository();
-		return new JdbcMemberRepository(dataSource);
+		//return new JdbcMemberRepository(dataSource);
+		//return new JdbcTemplateMemberRepositrory(dataSource);
+		return new JpaMemberRepository(em);
 	}
 
 }
--- spring_boot_project/boot2/src/main/java/kr/kh/boot2/service/MemberService.java
+++ spring_boot_project/boot2/src/main/java/kr/kh/boot2/service/MemberService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.kh.boot2.domain.Member;
 import kr.kh.boot2.repository.MemberRepository;
 
+@Transactional
 public class MemberService {
 	
 	private final MemberRepository memberRepository;
