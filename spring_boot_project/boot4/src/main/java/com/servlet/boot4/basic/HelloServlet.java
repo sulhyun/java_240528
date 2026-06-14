0220ed0fd2588c8680c6c1afb3745c6e97d35f45
sulhyun <sulhyun4788@naver.com>
2026-06-14 10:27:35 +0900

    파일 디렉토리 정리

--- spring_boot_project/boot4/src/main/java/com/servlet/boot4/basic/HelloServlet.java
+++ spring_boot_project/boot4/src/main/java/com/servlet/boot4/basic/HelloServlet.java
@@ -8,9 +8,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+// http://localhost:8080/hello?username=hello
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet extends HttpServlet{
-	
+public class HelloServlet extends HttpServlet {
+
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("HelloServlet.service");
@@ -24,4 +25,5 @@ public class HelloServlet extends HttpServlet{
 		response.setCharacterEncoding("utf-8");
 		response.getWriter().write("hello " + username);
 	}
+
 }
--- spring_boot_project/boot4/src/main/java/com/servlet/boot4/basic/request/RequestParamServlet.java
+++ spring_boot_project/boot4/src/main/java/com/servlet/boot4/basic/request/RequestParamServlet.java
@@ -9,40 +9,43 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/***
+ * 1. 피라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ * 
+ * 2. 동일한 피라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&username=bye&age=20
+ */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("[전체 파라미터 조회] - start");
-		
-		/*
-		Enumeration<String> paramNames = request.getParameterNames();
-		while(paramNames.hasMoreElements()) {
-			String paramName = paramNames.nextElement();
-			System.out.println(paramName + " = " + request.getParameter(paramName));
+		System.out.println("[전체 피라미터 조회]");
+		// 방법 1
+		Enumeration<String> parameterNames = request.getParameterNames();
+		while(parameterNames.hasMoreElements()) {
+			String parameterName = parameterNames.nextElement();
+			System.out.println(parameterName + ": " + request.getParameter(parameterName));
 		}
-		*/
 		
-		request.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + " = " + request.getParameter(paramName)));
-		
-		System.out.println("[전체 파라미터 조회] - end");
+		// 방법 2
+		request.getParameterNames().asIterator().forEachRemaining(parameterName -> System.out.println(parameterName + ": " + request.getParameter(parameterName)));
 		System.out.println();
 		
-		System.out.println("[단일 파라미터 조회]");
-		
+		System.out.println("[단일 피라미터 조회]");
 		String username = request.getParameter("username");
-		System.out.println("request.getParameter(username) = " + username);
+		System.out.println("request.getParameter(username): " + username);
 		
-		String age = request.getParameter("age");
-		System.out.println("request.getParameter(age) = " + age);
-		System.out.println();
+		int age = Integer.parseInt(request.getParameter("age"));
+		System.out.println("request.getParameter(age): " + age);
+		System.out.println();	
 		
-		System.out.println("[이름이 같은 복수 파라미터 조회]");
+		System.out.println("[이름이 같은 복수 피라미터 조회]");
 		System.out.println("request.getParameterValues(username)");
 		String[] usernames = request.getParameterValues("username");
 		for(String name : usernames) {
-			System.out.println("username = " + name);
+			System.out.println("username: " + name);
 		}
 		
 		response.getWriter().write("ok");
