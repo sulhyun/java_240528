b728acf87387bd590e139c517d47acfe673435c4
sulhyun <sulhyun4788@naver.com>
2026-06-28 19:49:37 +0900

    스프링 MVC 컨트롤러 V3

--- spring_boot_project/boot4/src/main/java/com/servlet/boot4/web/springmvc/v3/SpringMemberControllerV3.java
+++ spring_boot_project/boot4/src/main/java/com/servlet/boot4/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,12 +18,12 @@ public class SpringMemberControllerV3 {
 
 	private MemberRepository memberRepository = MemberRepository.getInstance();
 	
-	@RequestMapping("/form")
+	@GetMapping("/form")
 	public String form() {
 		return "form";
 	}
 	
-	@RequestMapping("/save")
+	@PostMapping("/save")
 	public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
 		Member member = new Member(username, age);
 		memberRepository.save(member);
@@ -30,7 +32,7 @@ public class SpringMemberControllerV3 {
 		return "save";
 	}
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Member> members = memberRepository.findAll();
 		
