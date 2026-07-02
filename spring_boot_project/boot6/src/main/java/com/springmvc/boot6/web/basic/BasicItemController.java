e869724d8a4f1e29fc98b557ba2641f3ceac4401
sulhyun <sulhyun4788@naver.com>
2026-07-02 20:24:47 +0900

    상품 타임리프

--- spring_boot_project/boot6/src/main/java/com/springmvc/boot6/web/basic/BasicItemController.java
+++ spring_boot_project/boot6/src/main/java/com/springmvc/boot6/web/basic/BasicItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springmvc.boot6.domain.Item;
@@ -21,10 +22,17 @@ public class BasicItemController {
 	private final ItemRepository itemRepository;
 	
 	@GetMapping
-	public String item(Model model) {
+	public String items(Model model) {
 		List<Item> items = itemRepository.findAll();
 		model.addAttribute("items", items);
 		return "basic/items";
+	}
+	
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "/basic/item";
 	}
 	
 	/**
