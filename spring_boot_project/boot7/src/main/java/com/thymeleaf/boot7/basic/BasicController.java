a0d08f9de7b0d5deedeed9c43ce4943f865038c5
sulhyun <sulhyun4788@naver.com>
2026-07-04 03:47:18 +0900

    타임리프 - 변수

--- spring_boot_project/boot7/src/main/java/com/thymeleaf/boot7/basic/BasicController.java
+++ spring_boot_project/boot7/src/main/java/com/thymeleaf/boot7/basic/BasicController.java
@@ -1,9 +1,16 @@
 package com.thymeleaf.boot7.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,6 +26,39 @@ public class BasicController {
 	public String utextBasic(Model model) {
 		model.addAttribute("data", "<b>Hello Spring!</b>");
 		return "basic/utext-basic";
+	}
+	
+	@GetMapping("/variable")
+	public String variable(Model model) { 
+		User userA = new User("userA", 10);
+		User userB = new User("userA", 20);
+		
+		List<User> list = new ArrayList<>();
+		list.add(userA);
+		list.add(userB);
+		
+		Map<String, User> map = new HashMap<>();
+		map.put("userA", userA);
+		map.put("userB", userB);
+		
+		model.addAttribute("user", userA);
+		model.addAttribute("list", list);
+		model.addAttribute("map", map);
+		
+		return "basic/variable";
+	}
+	
+	@Data
+	public static class User {
+		
+		private String username;
+		private int age;
+		
+		public User(String username, int age) { 
+			this.username = username;
+			this.age = age;
+		}
+		
 	}
 	
 }
