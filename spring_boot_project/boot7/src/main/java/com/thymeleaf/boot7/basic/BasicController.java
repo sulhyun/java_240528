b983815c4fd045c4693162683a84ff73571c9998
sulhyun <sulhyun4788@naver.com>
2026-07-04 15:28:16 +0900

    타임리프 - 리터럴

--- spring_boot_project/boot7/src/main/java/com/thymeleaf/boot7/basic/BasicController.java
+++ spring_boot_project/boot7/src/main/java/com/thymeleaf/boot7/basic/BasicController.java
@@ -76,6 +76,12 @@ public class BasicController {
 		return "basic/link";
 	}
 	
+	@GetMapping("/literal")
+	public String literal(Model model) {
+		model.addAttribute("data", "spring!");
+		return "basic/literal";
+	}
+	
 	@Component("bean")
 	static class Bean {
 		
