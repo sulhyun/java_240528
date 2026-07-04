ea728afa1ba1d306bb61fc25f3833b0929dc6a63
sulhyun <sulhyun4788@naver.com>
2026-07-05 00:14:52 +0900

    타임리프 - 템플릿 조각

---
+++ spring_boot_project/boot7/src/main/java/com/thymeleaf/boot7/basic/TemplateController.java
@@ -0,0 +1,17 @@
+package com.thymeleaf.boot7.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+	@GetMapping("/fragment")
+	public String fragment() {
+		return "template/fragment/fragmentMain";
+	}
+	
+	
+}
