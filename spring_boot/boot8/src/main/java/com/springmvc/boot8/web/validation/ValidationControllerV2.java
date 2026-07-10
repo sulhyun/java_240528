5937cc865be2750d2f9e05754b8ce2bc587e82dc
sulhyun <sulhyun4788@naver.com>
2026-07-10 22:54:24 +0900

    검증(Validation)
    Validator 분리

--- spring_boot/boot8/src/main/java/com/springmvc/boot8/web/validation/ValidationControllerV2.java
+++ spring_boot/boot8/src/main/java/com/springmvc/boot8/web/validation/ValidationControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
     
 	@ModelAttribute("regions")
 	public Map<String, String> regions() {
@@ -74,7 +76,7 @@ public class ValidationControllerV2 {
     	if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
     		bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
     	}
-    	if (item.getQuantity() == null || item.getQuantity() > 9999) {
+    	if (item.getQuantity() == null || item.getQuantity() > 10000) {
     		bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."));
     	}
     	
@@ -104,7 +106,7 @@ public class ValidationControllerV2 {
     	if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
     		bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,  "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
     	}
-    	if (item.getQuantity() == null || item.getQuantity() > 9999) {
+    	if (item.getQuantity() == null || item.getQuantity() > 10000) {
     		bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null,  "수량은 최대 9,999 까지 허용합니다."));
     	}
     	
@@ -136,7 +138,7 @@ public class ValidationControllerV2 {
     		// range.item.price=가격은 {0} ~ {1} 까지 허용합니다.
     		bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[] {"range.item.price"}, new Object[] {1000, 1000000},  null));
     	}
-    	if (item.getQuantity() == null || item.getQuantity() > 9999) {
+    	if (item.getQuantity() == null || item.getQuantity() > 10000) {
     		// max.item.quantity=수량은 최대 {0} 까지 허용합니다.
     		bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[] {"max.item.quantity"}, new Object[] {9999},  null));
     	}
@@ -160,7 +162,7 @@ public class ValidationControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
     
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
     	log.info("objectName: {}", bindingResult.getObjectName());
     	log.info("target: {}", bindingResult.getTarget());
@@ -173,7 +175,7 @@ public class ValidationControllerV2 {
     		// range.item.price=가격은 {0} ~ {1} 까지 허용합니다.
     		bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
     	}
-    	if (item.getQuantity() == null || item.getQuantity() > 9999) {
+    	if (item.getQuantity() == null || item.getQuantity() > 10000) {
     		// max.item.quantity=수량은 최대 {0} 까지 허용합니다.
     		bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
     	}
@@ -196,10 +198,25 @@ public class ValidationControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+    
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    	itemValidator.validate(item, bindingResult);
+
+    	if (bindingResult.hasErrors()) {
+    		log.info("errors={}", bindingResult);
+    		return "validation/v2/addForm";
+    	}
+    	
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
+		Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "validation/v2/editForm";
     }
