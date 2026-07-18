d908b01ebd0b60403ecd948fd9444e7c1b77714d
sulhyun <sulhyun4788@naver.com>
2026-07-18 09:47:51 +0900

    HTTP Session

--- spring_boot/boot10/src/main/java/com/login/boot10/home/HomeController.java
+++ spring_boot/boot10/src/main/java/com/login/boot10/home/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.login.boot10.member.domain.Member;
 import com.login.boot10.member.repository.MemberRepository;
@@ -58,7 +59,7 @@ public class HomeController {
     	return "loginHome";
     }
     
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
     	// 세션이 없으면 home
     	HttpSession session = request.getSession(false);
@@ -70,6 +71,17 @@ public class HomeController {
     	// 세션에 회원 데이터가 없으면 home
     	Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
     	
+    	if (loginMember == null) {
+    		return "home";
+    	}
+    	
+    	// 세션이 유지되면 로그인으로 이동
+    	model.addAttribute("member", loginMember);
+    	return "loginHome";
+    }
+    
+    @GetMapping("/")
+    public String homeLoginV4(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required =  false) Member loginMember, Model model) {
     	if (loginMember == null) {
     		return "home";
     	}
