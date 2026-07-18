284a9313b7879386270e777abfa98906ffb9a5ab
sulhyun <sulhyun4788@naver.com>
2026-07-18 14:31:46 +0900

    서블릿 필터
    요청 로그

---
+++ spring_boot/boot10/src/main/java/com/login/boot10/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.login.boot10.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.login.boot10.web.filter.LogFilter;
+
+import jakarta.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public FilterRegistrationBean<Filter> logFilter() {
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new LogFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+	
+}
