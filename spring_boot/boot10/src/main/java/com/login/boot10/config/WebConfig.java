c8c3203ac13c76dcd7e5f553312b1c610cafe5af
sulhyun <sulhyun4788@naver.com>
2026-07-18 17:52:11 +0900

    스프링 인터셉터
    요청 로그

--- spring_boot/boot10/src/main/java/com/login/boot10/config/WebConfig.java
+++ spring_boot/boot10/src/main/java/com/login/boot10/config/WebConfig.java
@@ -3,16 +3,27 @@ package com.login.boot10.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.login.boot10.web.filter.LogFilter;
 import com.login.boot10.web.filter.LoginCheckFilter;
+import com.login.boot10.web.interceptor.LogInterceptor;
 
 import jakarta.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "/*.ico", "/error");
+	}
+
+	// @Bean
 	public FilterRegistrationBean<Filter> logFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LogFilter());
