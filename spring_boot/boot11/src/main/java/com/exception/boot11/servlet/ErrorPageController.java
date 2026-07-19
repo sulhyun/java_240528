969956fd62d6d5c53b450bbfa6b1dc8e3a0358fe
sulhyun <sulhyun4788@naver.com>
2026-07-19 13:14:40 +0900

    API 예외 처리

--- spring_boot/boot11/src/main/java/com/exception/boot11/servlet/ErrorPageController.java
+++ spring_boot/boot11/src/main/java/com/exception/boot11/servlet/ErrorPageController.java
@@ -1,8 +1,15 @@
 package com.exception.boot11.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -33,10 +40,25 @@ public class ErrorPageController {
 		return "error-page/500";
 	}
 	
+	@RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Object>> errorPage500Api(
+			HttpServletRequest request, HttpServletResponse response) {
+		log.info("API errorPage 500");
+		
+		Map<String, Object> result = new HashMap<>();
+		Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+		result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+		result.put("message", ex.getMessage());
+		
+		Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
+	}
+	
 	private void printErrorInfo(HttpServletRequest request) {
 		log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
 		log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
-		log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); //ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
+		log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE)); // ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
 		log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
 		log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
 		log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
